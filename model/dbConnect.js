8b15e2e90d0408d0a20aede6b322da58f4966dcf
James Lee <danghuong18@gmail.com>
2021-08-09 16:30:58 +0700

    edit dbConnect

--- model/dbConnect.js
+++ model/dbConnect.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-console.log(process.env.DB_USERNAME);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_DBNAME);
-
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.0i8ec.mongodb.net/${process.env.DB_DBNAME}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
